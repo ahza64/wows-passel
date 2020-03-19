aa7bc70113cf2f6bb6b811c496cbcc25473473ea
ahza64 <justinmichaelpettit@gmail.com>
2020-03-19 16:32:01 -0700

    live ping server test

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 var port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/ping', function (req, res) {
-//   console.log('pong');
-//   return res.send('pong');
-// });
+app.get('/ping', function (req, res) {
+  console.log('pong');
+  return res.send('pong');
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log('server is runing on 8080'));
+app.listen(port, () => console.log('server is runing on ' + port));
