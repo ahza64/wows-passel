60f7ff74667a6bb4f8ebe7a78e17c21d411c421c
ahza64 <justinmichaelpettit@gmail.com>
2020-03-19 16:20:04 -0700

    add catch all route to server

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const app = express();
 var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
-  console.log('pong');
-  return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//   console.log('pong');
+//   return res.send('pong');
+// });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get(['/*'], function (req, res) {
+  console.log("base url redirect");
+  res.redirect(req.baseUrl + '/');
 });
 
 app.listen(port, () => console.log('server is runing on 8080'));
