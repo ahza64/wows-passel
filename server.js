3885793bc356850a25d85ec558e13dac3f16fee6
ahza64 <justinmichaelpettit@gmail.com>
2020-03-19 23:28:21 -0700

    make a request to /api/wg to fetch WG api data, parse the data, and send it to react in chart json format

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const fetch = require('node-fetch');
 const app = express();
 var port = process.env.PORT || 8080;
 
+require('dotenv').config();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
-  console.log('pong');
-  return res.send('pong');
+
+
+var url = 'https://api.worldofwarships.com/wows/encyclopedia/ships/?type=Destroyer&nation=japan&fields=name%2C+default_profile.concealment&application_id=' + process.env.WOWS_APP_ID;
+var jsonn = {};
+
+app.get('/api/wg', function (req, res) {
+  var names = [];
+  var concealments = [];
+  fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    var data = data.data;
+    console.log("ships from WG api", data);
+    for (const prop in data) {
+      console.log(data[prop].name);
+      names.push(data[prop].name);
+      concealments.push(data[prop].default_profile.concealment.detect_distance_by_ship);
+    }
+  })
+  .then(data => {
+    const state = {
+      labels: names,
+      datasets: [
+        {
+          label: 'Concealment',
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: concealments
+        }
+      ]
+    }
+    res.send(state);
+  })
+  .catch(err => {
+      console.log(err);
+  });
+
 });
 
 app.get('/', function (req, res) {
--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,66 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
 
-const state = {
-  labels: ["Arashi", "[Shimakaze]", "AL Yukikaze", "HSF Harekaze", "Shinonome",
-    "Asashio B", "Asashio", "Hayate", "Yūdachi", "Kitakaze", "Harugumo",
-    "Yūgumo", "Kagerō", "Shiratsuyu", "Hatsuharu", "Akizuki", "Akatsuki",
-    "Fubuki", "Mutsuki", "Kamikaze R", "Kamikaze", "Wakatake", "Fūjin",
-    "Tachibana Lima", "Shimakaze", "Minekaze", "Isokaze", "Umikaze", "Tachibana"],
-  datasets: [
-    {
-      label: 'Concealment',
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [6.8, 7.1, 6.8, 6.8, 7, 6.8, 6.8, 7.7, 7, 7.6, 7.9, 7, 6.8, 6.7,
-        6.7, 7.8, 7.4, 7, 6.2, 6.2, 6.2, 6.1, 6.2, 5.4, 7.1, 6.2, 6.1, 5.6, 5.4]
-    }
-  ]
-}
+// const state = {
+//   labels: ["Arashi", "[Shimakaze]", "AL Yukikaze", "HSF Harekaze", "Shinonome",
+//     "Asashio B", "Asashio", "Hayate", "Yūdachi", "Kitakaze", "Harugumo",
+//     "Yūgumo", "Kagerō", "Shiratsuyu", "Hatsuharu", "Akizuki", "Akatsuki",
+//     "Fubuki", "Mutsuki", "Kamikaze R", "Kamikaze", "Wakatake", "Fūjin",
+//     "Tachibana Lima", "Shimakaze", "Minekaze", "Isokaze", "Umikaze", "Tachibana"],
+//   datasets: [
+//     {
+//       label: 'Concealment',
+//       backgroundColor: 'rgba(75,192,192,1)',
+//       borderColor: 'rgba(0,0,0,1)',
+//       borderWidth: 2,
+//       data: [6.8, 7.1, 6.8, 6.8, 7, 6.8, 6.8, 7.7, 7, 7.6, 7.9, 7, 6.8, 6.7,
+//         6.7, 7.8, 7.4, 7, 6.2, 6.2, 6.2, 6.1, 6.2, 5.4, 7.1, 6.2, 6.1, 5.6, 5.4]
+//     }
+//   ]
+// }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        labels: ["Arashi"],
+        datasets: [
+          {
+            label: 'Concealment',
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [6.8]
+          }
+        ]
+      }
+    }
+  }
+
+  componentDidMount() {
+    fetch("/api/wg")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      this.setState({
+        data: data
+      });
+    })
+    .catch( err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
     <div>
-      <h1>React is loading with charts.js and node express</h1>
+      <h1>Loading WG live data on nodeJS with react!!</h1>
         <Bar
-          data={state}
+          data={this.state.data}
           options={{
             title:{
               display:true,
