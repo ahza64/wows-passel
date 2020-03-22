6591b81d0e0409145b8a88fcc37470ba942c5532
ahza64 <justinmichaelpettit@gmail.com>
2020-03-22 16:29:41 -0700

    moved some html around and changed some confusing text

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,16 @@ class App extends React.Component {
       hedps: [288],
       type: "Cruiser",
       nation: "usa",
-      field: "concealments",
+      field: "sample, click buttons above to retireve and render more data",
       chartData: {
-        labels: ["Arashi"],
+        labels: ["Erie", "Chester", "Albany", "St Louis", "Atlanta"],
         datasets: [
           {
             label: 'Concealment',
             backgroundColor: 'rgba(75,192,192,1)',
             borderColor: 'rgba(0,0,0,1)',
             borderWidth: 2,
-            data: [6.8]
+            data: [8.3, 8.5, 8.8, 11, 11]
           }
         ]
       }
@@ -121,33 +121,38 @@ class App extends React.Component {
   render() {
     return (
     <div>
-      <h1>full cycle external api,</h1>
-      <h3>enter values in the form and submit to retrive and graph concealment values from WG api</h3>
+      <h1>World of Warships data visualisation</h1>
+      <p>
+        click a button, to retrive and graph data from WG api
+      </p>
       <form onSubmit={e => this.onFormSubmit(e)}>
         <div>
           <div>
-          <label>class</label>
+          <div>
+          Click a value
+          </div>
+          <button key="1" onClick={e => this.onFormConcealmentSubmit(e)}>
+          Concealment
+          </button>
+          <button key="2" onClick={e => this.onFormHEDPMSubmit(e)}>
+          HE dpm
+          </button>
+          </div>
+          <div>
+          <label>Class: </label>
           <input
             type="text"
             value={this.state.type}
             onChange={e => this.setState({ type: e.target.value })}
-          /> Destroyer, Cruiser, Battleship, AirCarrier
+          /> Valid entries: Destroyer, Cruiser, Battleship, AirCarrier
           </div>
           <div>
-          <label>nation</label>
+          <label>Nation: </label>
           <input
             type="text"
             value={this.state.nation}
             onChange={e => this.setState({ nation: e.target.value })}
-          />commonwealth, europe, italy, usa, pan_asia, france, ussr, germany, uk, japan, pan_america
-          </div>
-          <div>
-            <button key="1" onClick={e => this.onFormConcealmentSubmit(e)}>
-              Concealment
-            </button>
-            <button key="2" onClick={e => this.onFormHEDPMSubmit(e)}>
-            HE dpm
-            </button>
+          /> Valid entires: commonwealth, europe, italy, usa, pan_asia, france, ussr, germany, uk, japan, pan_america
           </div>
         </div>
       </form>
