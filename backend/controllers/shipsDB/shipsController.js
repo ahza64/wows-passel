34a29603289f7c11bfa9e5bcd63556ad0bf99524
ahza64 <justinmichaelpettit@gmail.com>
2020-04-13 19:53:23 -0700

    implement express Router and file structure

---
+++ backend/controllers/shipsDB/shipsController.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const db = require('../../models/ship.js');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/ships/:type/:tier/:field', function (req, res) {
+  console.log("get ships pinged", req.params.type);
+  db.find({ tier: req.params.tier,  type: req.params.type})
+  .exec(function(err, ships){
+    if (err || !ships || !ships.length) {
+      return res.status(404).send({message: 'Ships not found.'});
+    }
+    console.log("ships", ships, `tier ${req.params.tier} class ${req.params.type}`);
+
+    res.send(ships);
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,22 +5,26 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 
 const app = express();
+const router = express.Router();
 
-const apiController = require('./backend/controllers/wgAPI/apiController.js');
+const apiWGController = require('./backend/controllers/wgAPI/apiWGController.js');
 const dbUpdateController = require('./backend/controllers/shipsDB/dbUpdateController.js');
-const shipController = require('./backend/controllers/shipsDB/shipController.js');
+// const shipsController = require('./backend/controllers/shipsDB/shipsController.js');
+const shipsRouter = require('./backend/controllers/shipsDB/shipsController.js');
 
 require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(router);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-apiController(app);
+apiWGController(app);
 dbUpdateController(app);
-shipController(app);
+// shipsController(app);
+router.get('/ships/:type/:tier/:field', shipsRouter)
 
 mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOHQ_URL ||
