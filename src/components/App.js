af83dabdc95046f5bbb8c2d84c324fb77415a0d6
ahza64 <justinmichaelpettit@gmail.com>
2020-05-18 13:19:46 -0700

    graph selection form and graph conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Col} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
 
@@ -11,7 +13,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-
+      graphType: "compile"
     }
   }
 
@@ -26,14 +28,33 @@ class App extends React.Component {
     });
   }
 
+  handleGraphTypeChange(e) {
+    e.preventDefault();
+    e.persist();
+    this.setState({graphType: e.target.value});
+  }
+
   render() {
     return (
     <div id="background" >
       <Container>
-        <h1 id="title">World of Warships data visualisation
+        <h1 id="title">
           <Button variant="outline-secondary" onClick={e => this.updateDB(e)}>update DB</Button>
+          World of Warships data visualisation
+          <Form >
+          <Form.Row>
+          <Form.Group as={Col} controlId="formGridCity">
+          <Form.Control name="type" as="select" onChange={e => this.handleGraphTypeChange(e)} value={this.state.graphType}>
+          <option value="compile">Compile Graph</option>
+          <option value="compare">Compare Graph</option>
+          </Form.Control>
+          </Form.Group>
+          </Form.Row>
+          </Form>
         </h1>
-        <CompileGraph />
+        {this.state.graphType === "compile" &&
+          <CompileGraph />
+        }
       </Container>
     </div>
 
